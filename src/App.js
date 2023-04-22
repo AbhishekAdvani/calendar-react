7579177146362ee990fa97f7065f50162ec13a7a
abhishekadvani <a9819191140@gmail.com>
2023-04-22 21:12:21 +0530

    react-calendar-code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "./App.css";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>React Calendar</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} selectRange={true} />
+      </div>
+
+      {date.length > 0 ? (
+        <p className="text-center">
+          <span className="bold">Start:</span> {date[0].toDateString()}
+          &nbsp;|&nbsp;
+          <span className="bold">End:</span> {date[1].toDateString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <p className="text-center">
+          <span className="bold">Default selected date:</span>{" "}
+          {date.toDateString()}
+        </p>
+      )}
     </div>
   );
 }
